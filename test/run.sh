af6b6c2f104f4443491c57c344224160aaa758ca
anzerr <ryan.anzer@epitech.eu>
2018-01-11 14:24:37 +0100

    save

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,10 @@
+
 mkdir -p ../build/
 rm ../build/server ../build/client
+cd ../lib/lib_src/
+make re
+cp libmy.a ../../server/
+cp libmy.a ../../client/c/
 cd ../server/
 make re
 cp server ../build/
